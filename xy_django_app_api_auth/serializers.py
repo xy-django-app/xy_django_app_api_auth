550aa0964a1d8af8a6c71e70b4c01a9dbce6cbc3
yuyanget <845262968@qq.com>
2024-10-31 21:39:17 +0800

    fix 修改解析器等

--- xy_django_app_api_auth/serializers.py
+++ xy_django_app_api_auth/serializers.py
@@ -1,17 +1,20 @@
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 
 
 from .models import *
 from rest_framework import viewsets
-from xy_admin_model_serializer.Admin_ModelSerializer import *
+from xy_django_serializer.Serializer import *
+
 
 # Serializers define the API representation.
-class ApiAuthCredentialSerializer(Admin_ModelSerializer):
+class ApiAuthCredentialSerializer(Serializer):
 
     default_value = ""
+
     class Meta:
         model = ApiAuthCredential
-        fields = '__all__'
+        fields = "__all__"
+
 
 # ViewSets define the view behavior.
 class ApiAuthCredentialViewSet(viewsets.ModelViewSet):
@@ -20,10 +23,10 @@ class ApiAuthCredentialViewSet(viewsets.ModelViewSet):
 
 
 # Serializers define the API representation.
-class ApiAuthCredentialCacheSerializer(Admin_ModelSerializer):
+class ApiAuthCredentialCacheSerializer(Serializer):
 
     default_value = ""
-    
+
     class Meta:
         model = ApiAuthCredentialCache
         fields = "__all__"
@@ -33,4 +36,3 @@ class ApiAuthCredentialCacheSerializer(Admin_ModelSerializer):
 class ApiAuthCredentialCacheViewSet(viewsets.ModelViewSet):
     queryset = ApiAuthCredentialCache.objects.all()
     serializer_class = ApiAuthCredentialCacheSerializer
-
